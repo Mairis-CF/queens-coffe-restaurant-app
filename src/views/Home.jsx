9c8b17d42fd438668c3654dd647091e72c3575ca
Mairis <mairiscastroflorez@gmail.com>
2021-12-14 22:16:07 -0500

    Cambiando sistema de archivos y configurando package.json

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,21 +1,17 @@
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-    return (
-        <div>
-            <h2>Home view</h2>
-            <button>
-                <Link to='/Waiter'>
-                    Tables
-                </Link>
-            </button>
-            <button>
-                <Link to='/Cook'>
-                    Cook
-                </Link>
-            </button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Home view</h2>
+      <button>
+        <Link to='/Waiter'>Tables</Link>
+      </button>
+      <button>
+        <Link to='/Cook'>Cook</Link>
+      </button>
+    </div>
+  );
+};
 
 export default Home;
